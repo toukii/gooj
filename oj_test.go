9b0dafb7c003d4199e70e13b705bc609f78d81be
shaalx <wisdomswing@gmail.com>
2015-09-04 09:18:15 +0800

    test

--- oj_test.go
+++ oj_test.go
@@ -2,9 +2,65 @@ package gooj
 
 import (
 	"github.com/qiniu/log"
+	"reflect"
 	"testing"
 	"time"
 )
+
+func Oj(result chan string) {
+	retc := make(chan bool)
+	t1 := time.Now()
+	go func() {
+		for _, it := range testcases {
+			ret := FUNC(it.in)
+			if !reflect.DeepEqual(ret, it.out) {
+				retc <- false
+				log.Println(it.in)
+				log.Println("want", it.out)
+				log.Println("get", ret)
+				return
+			}
+			retc <- true
+		}
+	}()
+	length := len(testcases)
+	ticker := time.NewTicker(5e8)
+	for i := 0; i < length; i++ {
+		select {
+		case <-ticker.C:
+			result <- "TIMEOUT"
+		case ok := <-retc:
+			if !ok {
+				result <- "WA"
+				return
+			}
+		}
+	}
+	log.Info(time.Now().Sub(t1))
+	result <- "AC"
+}
+
+type TestCases struct {
+	in  []int
+	out []int
+}
+
+var testcases []TestCases
+
+func init() {
+	testcases = []TestCases{
+		{in: []int{1, 2, 3}, out: []int{3, 2, 1}},
+		{in: []int{1, 2, 4}, out: []int{4, 2, 1}},
+		{in: []int{1, 5, 3}, out: []int{3, 5, 1}},
+		{in: []int{6, 2, 3}, out: []int{3, 2, 6}},
+	}
+}
+
+func TestOj(t *testing.T) {
+	result := make(chan string)
+	go Oj(result)
+	log.Info(<-result)
+}
 
 func Hack() {
 	for {
@@ -12,37 +68,15 @@ func Hack() {
 	}
 }
 
-func FUNC(arg ...int) []int {
-	Hack()
-	ret := make([]int, 3)
-	ret[0], ret[1], ret[2] = 1, 2, 3
-	time.Sleep(1e8)
-	return ret
-}
-
-func Oj(result chan string) {
-	retc := make(chan bool)
-	t1 := time.Now()
-	go func() {
-		ret := FUNC()
-		log.Println(ret)
-		retc <- true
-	}()
-
-	select {
-	case <-time.After(5e8):
-		result <- "TIMEOUT"
-	case <-retc:
-		log.Info(time.Now().Sub(t1))
-		result <- "AC"
+func FUNC(in []int) []int {
+	// Hack()
+	leng := len(in)
+	l := leng / 2
+	for i := 0; i < l; i++ {
+		in[i], in[leng-1-i] = in[leng-1-i], in[i]
 	}
+	return in
 }
-
-// func TestOj(t *testing.T) {
-// 	result := make(chan string)
-// 	go Oj(result)
-// 	log.Info(<-result)
-// }
 
 func TestRender(t *testing.T) {
 	content := `package goojt
