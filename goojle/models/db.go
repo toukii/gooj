5036e11507e164d09ec8a1bc7768629b5869a3e1
shaalx <shaalx@yahoo.com.sg>
2016-04-06 09:37:08 +0800

    dao

--- goojle/models/db.go
+++ goojle/models/db.go
@@ -9,11 +9,11 @@ import (
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	// orm.RegisterDataBase("localhost", "mysql", "root:1234@tcp(localhost:3306)/session?charset=utf8")
-	orm.RegisterDataBase("dao", "mysql", "uEIYt69vVZXOJDok:pQCautZUg6ji0fdmL@tcp(10.10.26.58:3306)/k3MHIXiO61hr5vC0?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "uEIYt69vVZXOJDok:pQCautZUg6ji0fdmL@tcp(10.10.26.58:3306)/k3MHIXiO61hr5vC0?charset=utf8")
 	// orm.RegisterDataBase("default", "mysql", "goojle:Goojle123@tcp(121.42.161.248:3306)/gooj?charset=utf8")
 	orm.RegisterModel(new(User), new(Puzzle), new(Solution), new(Remark), new(Result))
 	ORM = orm.NewOrm()
-	ORM.Using("dao")
+	ORM.Using("default")
 	orm.Debug = true
 }
 
