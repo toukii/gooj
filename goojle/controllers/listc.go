72445d9cb6063638f6f5f96994f9b185c01b9741
shaalx <shaalx@yahoo.com.sg>
2016-04-07 14:21:48 +0800

    forge

--- goojle/controllers/listc.go
+++ goojle/controllers/listc.go
@@ -13,7 +13,7 @@ type JudgeController struct {
 // @router / [get]
 func (c *JudgeController) Get() {
 	var puzzles []models.Puzzle
-	n, err := models.ORM.QueryTable((*models.Puzzle)(nil)).RelatedSel().Filter("Online", 1).Limit(10, 0).All(&puzzles)
+	n, err := models.ORM.QueryTable((*models.Puzzle)(nil)).RelatedSel().Filter("Online", 1).Limit(15, 0).All(&puzzles)
 	beego.Debug(n, err)
 	c.Data["title"] = "Puzzle"
 	puzzlez := make([]models.Puzzle, 0, len(puzzles))
@@ -29,7 +29,7 @@ func (c *JudgeController) Get() {
 // @router /state [get]
 func (c *JudgeController) State() {
 	var solutions []models.Solution
-	n, err := models.ORM.QueryTable((*models.Solution)(nil)).RelatedSel().OrderBy("-created").Limit(12).All(&solutions)
+	n, err := models.ORM.QueryTable((*models.Solution)(nil)).RelatedSel().OrderBy("-created").Limit(15).All(&solutions)
 	beego.Debug(n, err)
 	c.Data["title"] = "State"
 	c.Data["solutions"] = solutions
