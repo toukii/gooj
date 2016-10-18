48fd3c537626a5c5bd2f7fa45664666940b3f657
toukii <toukii@yahoo.com>
2016-10-18 20:22:17 +0800

    header

--- goojle/routers/router.go
+++ goojle/routers/router.go
@@ -9,4 +9,5 @@ func init() {
 	// beego.Router("/", &controllers.MainController{})
 	beego.Include(&controllers.JudgeController{}, &controllers.LoginController{}, &controllers.RegistController{}, &controllers.PuzzleController{}, &controllers.UserController{}, &controllers.ListController{})
 	beego.ErrorController(&controllers.ErrorController{})
+	beego.SetStaticPath("public", "static")
 }
